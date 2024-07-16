0e4e0a770974614cfbd0c8724d2c86e5f871ae48
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2024-07-16 11:32:51 -0300

    security configuration

--- gateway/src/main/java/com/david/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/david/gateway/GatewayApplication.java
@@ -1,7 +1,16 @@
 package com.david.gateway;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -10,4 +19,17 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+}
+
+@RestController
+class ProfileController {
+	@GetMapping("userinfo")
+	public Map<String, Object> userInfo(@AuthenticationPrincipal OidcUser oidcUser,
+			@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
+		var attributesMap = new HashMap<>(oidcUser.getAttributes());
+		attributesMap.put("token", oidcUser.getIdToken().getTokenValue());
+		attributesMap.put("clientName", authorizedClient.getClientRegistration().getClientId());
+		attributesMap.put("userAttributes", oidcUser.getAttributes());
+		return attributesMap;
+	}
 }
