699399d9f12111235daf701f068ef9e12a076765
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2024-07-16 08:58:55 -0300

    create gateway project

--- secured-service/src/main/java/com/david/secured_service/SecuredServiceApplication.java
+++ secured-service/src/main/java/com/david/secured_service/SecuredServiceApplication.java
@@ -2,6 +2,10 @@ package com.david.secured_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SecuredServiceApplication {
@@ -10,4 +14,18 @@ public class SecuredServiceApplication {
 		SpringApplication.run(SecuredServiceApplication.class, args);
 	}
 
+}
+
+@RestController
+class ResourceController {
+	@GetMapping("resource")
+	public String getResource(@AuthenticationPrincipal Jwt jwt) {
+		var strBuilder = new StringBuilder();
+		strBuilder.append("<p>JWT Token: %s</p>".formatted(jwt.getTokenValue()));
+		strBuilder.append("<p>JWT Headers: %s</p>".formatted(jwt.getHeaders()));
+		strBuilder.append("<p>JWT Claims: %s</p>".formatted(jwt.getClaims().toString()));
+		strBuilder.append(
+				"<p>Resource accessed by: %s (with subject: %s)</p>".formatted(jwt.getSubject(), jwt.getSubject()));
+		return strBuilder.toString();
+	}
 }
